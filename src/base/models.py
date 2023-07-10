16d1fe65c39908f02e0ab6af758096f8ccfa948b
Hongwei-Yu <62824819+Hongwei-Yu@users.noreply.github.com>
2023-07-10 23:45:08 +0800

    Initial commit

--- src/base/models.py
+++ src/base/models.py
@@ -5,13 +5,13 @@ from pydantic import BaseModel
 
 class TestInfo(BaseModel):
     """用例信息"""
-    测试用例编号: float
+    测试用例编号: str
     测试用例名: str
 
 
 class TestStep(BaseModel):
     """用例步骤"""
-    步骤序号:float
+    步骤序号: str
     步骤名: str
     关键字: str
     元素标识: Optional[str]
@@ -28,4 +28,5 @@ class BaseTestCase(BaseModel):
 
 class BaseTestSuite(BaseModel):
     """测试套件"""
+    name: str
     case_list: List[BaseTestCase]
